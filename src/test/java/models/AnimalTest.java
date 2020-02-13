0f424f25d76643f922701059c739f07ba41f798e
JosehGaks <josehwilddog@gmail.com>
2020-02-13 10:25:10 +0300

    create tests for animal

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,27 @@
 package models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 class AnimalTest {
-
+    @Test
+    public void animal_instatiatesCorrectly_true(){
+        Animal testAnimal = new Animal("Lion");
+        assertEquals("Lion",testAnimal.getName());
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+        Animal firstAnimal = new Animal("Lion");
+        Animal anotherAnimal = new Animal("Lion");
+        assertTrue(firstAnimal.equals(anotherAnimal));
+    }
+    @Test
+    public void save_insertObjectIntoDatabase_Animal(){
+        Animal testAnimal = new Animal("Lion");
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
+    }
 }
